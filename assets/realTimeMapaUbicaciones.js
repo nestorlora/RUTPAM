53dab61159a232a5f0aabfdfb3e4328830722ade
nestorlora <nestorlora@gmail.com>
2018-08-06 17:20:34 +0200

    Cambio a versión 4.0

--- assets/realTimeMapaUbicaciones.js
+++ assets/realTimeMapaUbicaciones.js
@@ -433,7 +433,7 @@ function busInfoContent(Bus){
 
 function ControlRUTPAM(mapDiv){
 	var layer = $("<div>", {"id":"layer"});
-	var titulo = $("<p>").append($("<b>", {"text":"RUTPAM"}));
+	var titulo = $("<p>").append($("<b>", {"text":"RUTPAM"})).append($("<span>", {"text":" v4.0"}));
 	var descripcion = $("<p>", {"text":"Seguimiento buses EMT en tiempo real"});
 	$(layer).append(titulo).append(descripcion);
 	
