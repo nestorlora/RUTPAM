cb84b04d07d860ee2ed33b0dc98c48a020cd0536
nestorlora <nestorlora@gmail.com>
2018-08-19 12:02:10 +0200

    Enlazada información de línea con el listado

--- assets/realTimeMapaUbicaciones.js
+++ assets/realTimeMapaUbicaciones.js
@@ -345,9 +345,7 @@ function addLinea(lin){
 	$(fila).append($("<td>").append(botonVta));
 	$(fila).append($("<td>").append(botonBus));
 	$(fila).append($("<td>").append(id));
-	$(fila).append($("<td>", {
-		"text": linea.nombreLinea
-	}));
+	$(fila).append($("<td>").append($("<a>", {text: linea.nombreLinea})).click(function(){verInfoLínea(linea.codLinea);}));
 	$(fila).append($("<td>").append($("<p>").attr('id', "cont"+linea.codLinea)));
 
 	$("#tablaLineas").append(fila);
