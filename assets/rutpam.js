ae912ea3726f432c43bfb7023266b2e73f93ed88
nestorlora <nestorlora@geeklab.es>
2018-08-28 02:01:54 +0200

    Cambio de versión a 4.8
    Signed-off-by: nestorlora <nestorlora@geeklab.es>

--- assets/rutpam.js
+++ assets/rutpam.js
@@ -28,7 +28,7 @@
  * @description Variable global para la versión del programa
  * @type String
  */
-var rutpam_version = "4.7";
+var rutpam_version = "4.8";
 
 /**
  * @description Variable global para almacenar el timer maestro
